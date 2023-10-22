0961273bbd58c19b014be48bf24dc4382a21827c
Lluis <llpfdc@gmail.com>
2023-10-22 16:22:03 +0200

    tests nearest charger and charger info endpoints

--- goVolt/api/chargers/urls.py
+++ goVolt/api/chargers/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ChargerLocationApiView,ChargerDataBaseApiView
+from .views import ChargerLocationApiView,ChargerDataBaseApiView,ChargerApiView,NearestChargerApiView
 
 urlpatterns = [
-    path('',ChargerLocationApiView.as_view(),name='chargers-loc'),
-    path('database',ChargerDataBaseApiView.as_view(),name='chargers-database')
+    path('all',ChargerLocationApiView.as_view(),name='chargers-loc'),
+    path('database',ChargerDataBaseApiView.as_view(),name='chargers-database'),
+    path('nearest',NearestChargerApiView.as_view(),name='chargers-nearest'),
+    path('<str:id>',ChargerApiView.as_view(),name='charger-detail')
 ]
