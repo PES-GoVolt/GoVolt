a9332897fd995280efe9dd3256f9d4d9c2d55c21
Lluis <llpfdc@gmail.com>
2023-11-05 16:56:21 +0100

    fix

--- goVolt/api/chats/views.py
+++ goVolt/api/chats/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from .serializers import MessageSerializer
-from .services import save_message,get_all_messages
+from .services import save_message
 from rest_framework.response import Response
 
 
