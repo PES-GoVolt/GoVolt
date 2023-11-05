e6ec152cf9d5cfa4c2ef0c90045682dfc5f50d26
Lluis <llpfdc@gmail.com>
2023-11-05 17:08:46 +0100

    tests added

--- goVolt/api/chats/tests.py
+++ goVolt/api/chats/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from django.urls import reverse
+import json
 
-# Create your tests here.
+class ChatsAPIViewTest(TestCase):
+    def test_send_message(self):
+        client = APIClient()
+        url = reverse('chats-view')
+        data = {
+            "content" : "Hello",
+            "room_name" : "random_room",
+            "sender" : "Hi"
+        }
+        response = client.post(url,data, format= 'json')
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
--- goVolt/api/chats/urls.py
+++ goVolt/api/chats/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from .views import ChatsAPIView
-from .views import room
 urlpatterns = [
     path('',ChatsAPIView.as_view(),name='chats-view'),
-    path('templates/<str:room_name>',room,name='template_test')
 ]
