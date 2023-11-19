747c4f16278050cdfd526ec58d7add34be3c0c81
MarcChavez <castromarc2000@gmail.com>
2023-11-19 18:40:04 +0100

    don't look

--- goVolt/goVolt/urls.py
+++ goVolt/goVolt/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/bikestations/',include('api.bikestations.urls')),
     path('api/users/',include('api.users.urls')),
     path('api/messages/',include('api.chats.urls')),
-    #path('api/rutas/',include('api.rutas.urls'))
+    path('api/rutas/',include('api.rutas.urls'))
 ]
